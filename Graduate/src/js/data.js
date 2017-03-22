3466817270bd3005562e7d0982f81bd49ac00ad4
Sherry <xieshiyi7@gmail.com>
2017-03-22 23:51:22 +0800

    first commit

---
+++ Graduate/src/js/data.js
@@ -0,0 +1,268 @@
+$(function(){
+    //导航栏点击切换页面
+
+    $('.one').bind('click',function(event){
+        $('.nav_title').removeClass('active');
+        $('.one').addClass('active');
+        $('.main').css('display','none');
+        $('.pageOne').css('display','block');
+    });
+    $('.two').bind('click',function(event){
+        $('.nav_title').removeClass('active');
+        $('.two').addClass('active');
+        $('.main').css('display','none');
+        $('.pageTwo').css('display','block');
+    });
+    $('.three').bind('click',function(event){
+        $('.nav_title').removeClass('active');
+        $('.three').addClass('active');
+        $('.main').css('display','none');
+        $('.pageThree').css('display','block');
+    });
+    $('.four').bind('click',function(event){
+        $('.nav_title').removeClass('active');
+        $('.four').addClass('active');
+        $('.main').css('display','none');
+        $('.pageFour').css('display','block');
+    });
+    $('.five').bind('click',function(event){
+        $('.nav_title').removeClass('active');
+        $('.five').addClass('active');
+        $('.main').css('display','none');
+        $('.pageFive').css('display','block');
+    });
+     // 基于准备好的dom，初始化echarts实例
+        var lineChart = echarts.init(document.getElementById('line-chart'));
+        var barChart = echarts.init(document.getElementById('bar-chart'));
+        var radarChart = echarts.init(document.getElementById('radar-chart'));
+        var dashBoard = echarts.init(document.getElementById('dash-board'));
+
+        // 指定图表的配置项和数据
+        /**
+         * 折线图
+         */
+        function randomData() {
+            now = new Date(+now + oneDay);
+            value = value + Math.random() * 21 - 10;
+            return {
+                name: now.toString(),
+                value: [
+                    [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
+                    Math.round(value)
+                ]
+            }
+        }
+
+        var data = [];
+        var now = +new Date(1997, 9, 3);
+        var oneDay = 24 * 3600 * 1000;
+        var value = Math.random() * 1000;
+        for (var i = 0; i < 1000; i++) {
+            data.push(randomData());
+        }
+
+        option = {
+            title: {
+                text: '动态数据 + 时间坐标轴'
+            },
+            tooltip: {
+                trigger: 'axis',
+                formatter: function (params) {
+                    params = params[0];
+                    var date = new Date(params.name);
+                    return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
+                },
+                axisPointer: {
+                    animation: false
+                }
+            },
+            xAxis: {
+                type: 'time',
+                splitLine: {
+                    show: false
+                }
+            },
+            yAxis: {
+                type: 'value',
+                boundaryGap: [0, '100%'],
+                splitLine: {
+                    show: false
+                }
+            },
+            series: [{
+                name: '模拟数据',
+                type: 'line',
+                showSymbol: false,
+                hoverAnimation: false,
+                data: data
+            }]
+        };
+
+        setInterval(function () {
+
+            for (var i = 0; i < 5; i++) {
+                data.shift();
+                data.push(randomData());
+            }
+
+            lineChart.setOption({
+                series: [{
+                    data: data
+                }]
+            });
+        }, 1000);
+
+        /**
+         * 柱状图
+         */
+        // var option2 = {
+        //     title: {
+        //         text: '柱状图'
+        //     },
+        //     tooltip: {},
+        //     legend: {
+        //         data:['销量']
+        //     },
+        //     xAxis: {
+        //         data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        //     },
+        //     yAxis: {},
+        //     series: [{
+        //         name: '销量',
+        //         type: 'bar',
+        //         data: [5, 20, 36, 10, 10, 20]
+        //     }]
+        // };
+        var xAxisData = [];
+        var data1 = [];
+        var data2 = [];
+        for (var i = 0; i < 100; i++) {
+            xAxisData.push('类目' + i);
+            data1.push((Math.sin(i / 5) * (i / 5 -10) + i / 6) * 5);
+            data2.push((Math.cos(i / 5) * (i / 5 -10) + i / 6) * 5);
+        }
+
+        option2 = {
+            title: {
+                text: '柱状图动画延迟'
+            },
+            legend: {
+                data: ['bar', 'bar2'],
+                align: 'left'
+            },
+            toolbox: {
+                // y: 'bottom',
+                feature: {
+                    magicType: {
+                        type: ['stack', 'tiled']
+                    },
+                    dataView: {},
+                    saveAsImage: {
+                        pixelRatio: 2
+                    }
+                }
+            },
+            tooltip: {},
+            xAxis: {
+                data: xAxisData,
+                silent: false,
+                splitLine: {
+                    show: false
+                }
+            },
+            yAxis: {
+            },
+            series: [{
+                name: 'bar',
+                type: 'bar',
+                data: data1,
+                animationDelay: function (idx) {
+                    return idx * 10;
+                }
+            }, {
+                name: 'bar2',
+                type: 'bar',
+                data: data2,
+                animationDelay: function (idx) {
+                    return idx * 10 + 100;
+                }
+            }],
+            animationEasing: 'elasticOut',
+            animationDelayUpdate: function (idx) {
+                return idx * 5;
+            }
+        };
+
+        /**
+         * 雷达图
+         */
+        option3 = {
+            title: {
+                text: '基础雷达图'
+            },
+            tooltip: {},
+            legend: {
+                data: ['预算分配（Allocated Budget）', '实际开销（Actual Spending）']
+            },
+            radar: {
+                // shape: 'circle',
+                indicator: [
+                { name: '销售（sales）', max: 6500},
+                { name: '管理（Administration）', max: 16000},
+                { name: '信息技术（Information Techology）', max: 30000},
+                { name: '客服（Customer Support）', max: 38000},
+                { name: '研发（Development）', max: 52000},
+                { name: '市场（Marketing）', max: 25000}
+                ]
+            },
+            series: [{
+                name: '预算 vs 开销（Budget vs spending）',
+                type: 'radar',
+                // areaStyle: {normal: {}},
+                data : [
+                    {
+                        value : [4300, 10000, 28000, 35000, 50000, 19000],
+                        name : '预算分配（Allocated Budget）'
+                    },
+                    {
+                        value : [5000, 14000, 28000, 31000, 42000, 21000],
+                        name : '实际开销（Actual Spending）'
+                    }
+                ]
+            }]
+        };
+        /**
+         *仪表盘
+         */
+        option4 = {
+            tooltip : {
+                formatter: "{a} <br/>{b} : {c}%"
+            },
+            toolbox: {
+                feature: {
+                    restore: {},
+                    saveAsImage: {}
+                }
+            },
+            series: [
+                {
+                    name: '业务指标',
+                    type: 'gauge',
+                    detail: {formatter:'{value}%'},
+                    data: [{value: 50, name: '完成率'}]
+                }
+            ]
+        };
+
+        setInterval(function () {
+            option4.series[0].data[0].value = (Math.random() * 100).toFixed(2) - 0;
+            dashBoard.setOption(option4, true);
+        },2000);
+
+        // 使用刚指定的配置项和数据显示图表。
+        lineChart.setOption(option);
+        barChart.setOption(option2);
+        radarChart.setOption(option3);
+        dashBoard.setOption(option4);
+
+})
